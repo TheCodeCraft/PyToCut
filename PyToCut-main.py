7c68aea933a2daf1100eb6216fdd88d6cb5f1fa4
TheCodeCraft <89706070+TheCodeCraft@users.noreply.github.com>
2022-05-25 21:04:47 +0200

    Update and rename main.py to PyToCut-main.py

--- PyToCut-main.py
+++ PyToCut-main.py
@@ -1,11 +1,16 @@
+#imports
 import cv2
 import os
 
+#dir
 directory = "output"
 parent_dir = os.path.abspath(os.getcwd())
 path = os.path.join(parent_dir, directory)
+#creating the dir
 os.mkdir(path)
+#input video
 vid = cv2.VideoCapture(input("filename: "))
+#input frames
 frames = input("frames: ")
 i = 0
 i2 = 0
@@ -14,9 +19,11 @@ while vid.isOpened():
     if not ret:
         break
     if i % int(frames) == 0:
+        #writing
         cv2.imwrite(directory + '/frame' + str(i2) + '.png', frame)
         i2 += 1
     i += 1
-    print("File: " + str(i) + " Frame: " + str(i2))
+  print("File: " + str(i) + " Frame: " + str(i2))
+#close
 vid.release()
 cv2.destroyAllWindows()
